ca67888539b0092cde4926b615073f95ffd50cf5
Tushar Walavalkar <Tushar.W@WinWire.com>
2025-03-06 12:36:39 -0600

    Loading patients from API.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  patients: any | undefined;
+
+  http = inject(HttpClient);
   title = 'Frontend';
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7182/api/Patient?maxCount=20').subscribe({
+      next: (patients) => {
+        this.patients = patients;
+      },
+      error: (error) => console.error(error),
+      complete: () => console.debug('Patients retrieved.'),
+    });
+  }
 }
